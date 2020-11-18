070190688f540fc09cd45d1dd28900bb542116be
mobystudent <mobystudent@gmail.com>
2020-11-18 21:04:43 +0300

    Add slider in might-fit section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
 $(window).on('load', () => {
 	showFilter();
+	slider();
 });
 
 function showFilter() {
@@ -19,5 +23,36 @@ function showFilter() {
 		else {
 			$(this).removeClass('filter__header--active').next().slideUp();
 		}
-});
+	});
+}
+
+function slider() {
+	$("#slider").slick({
+		infinite: false,
+		speed: 1000,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		easing: 'ease-in',
+		dots: true,
+		appendDots: '.slider__dots',
+		prevArrow: '.slider__prev',
+		nextArrow: '.slider__next',
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			}
+		]
+	});
 }
