ff3372f57b52ea78f6e9db63c418585dc231d548
mobystudent <mobystudent@gmail.com>
2020-11-18 18:45:35 +0300

    Add logic show filter

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
 $(window).on('load', () => {
-
+	showFilter();
 });
+
+function showFilter() {
+	$('.filter__field--block').click(function(){
+		$(this).toggleClass('filter__field--active').next().slideToggle();
+	});
+
+	$('.filter__header--select').click(function(){
+		if(!$(this).hasClass('filter__header--active')) {
+			new Promise((resolve) => {
+				$('.filter__header--select').removeClass('filter__header--active').next().slideUp(250);
+				setTimeout(() => resolve(), 250);
+			}).then(() => {
+				$(this).addClass('filter__header--active').next().slideDown();
+			});
+	 	}
+		else {
+			$(this).removeClass('filter__header--active').next().slideUp();
+		}
+});
+}
